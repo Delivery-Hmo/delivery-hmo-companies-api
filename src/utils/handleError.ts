dd43095b116a5aa98550847450964d5a5ad429e3
fedeWax <cha_lino_23@hotmail.com>
2023-05-17 21:39:40 -0700

    mejora handle error

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -2,6 +2,10 @@ import { Response } from "express";
 
 export const unauthorized = (res: Response) => res.status(401).json({ message: 'Unauthorized' });
 
-const handleError = (res: Response, err: any) => res.status(500).json(`${err.message ?? err}`);
+const handleError = (res: Response, err: any) => {
+  console.log(err);
+
+  res.status(500).json(`${err.message ?? err}`)
+};
 
 export default handleError;
