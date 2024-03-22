47a17ef7222e7abacfd88fe8f4b5b01c6ac79197
fedeWax <cha_lino_23@hotmail.com>
2024-03-21 20:49:14 -0700

    Fix phone models constans

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -33,14 +33,6 @@ export const maxlengthImage = 1000;
 export const urlImageDefaultProfile = "https://firebasestorage.googleapis.com/v0/b/delivery-hmo.appspot.com/o/imagenesPerfil%2F1467646262_522853_1467646344_noticia_normal.jpg?alt=media&token=f6e761ad-95c5-462f-bc39-0e889ac30a5c";
 export const baseUrlStorage = "https://firebasestorage.googleapis.com/v0/b/delivery-hmo.appspot.com/o/";
 export const baseUrlStorageGoogle = "https://storage.googleapis.com/delivery-hmo.appspot.com/";
-export const phone: SchemaDefinitionProperty<number | undefined> = {
-  type: Number,
-  required: true,
-  validate: {
-    validator: (value: number) => value.toString().length === 10,
-    message: "El número telefónico tienen que ser de 10 dígitos."
-  }
-} as const;
 export const image: SchemaDefinitionProperty<string | undefined> = {
   type: String,
   maxlength: maxlengthImage,
